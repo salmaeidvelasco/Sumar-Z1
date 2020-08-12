79e651d348b5052c5be2d724159d80a4fc20d088
salmaeidvelasco <a2019115131@estudiantes.upsa.edu.bo>
2020-08-12 19:52:41 -0400

    Mi primer código

---
+++ Sumar-Restar/Sumar/Controllers/SumaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sumar.Controllers
+{
+    [Route("api/[controller]")] // para controlar nuestro servidor
+    [ApiController]
+    public class SumaController : ControllerBase
+    {
+        [HttpGet]
+        public int Add(int a, int b) //Creamos este método público donde también creamos los elementos
+        {
+            return a + b; //Una vez éstos sumados, enviamos la suma 
+        }
+
+        [HttpPost]
+        public int Add2([FromHeader]int a, [FromHeader]int b) //Creamos este método público donde también creamos los elementos
+        {
+            return a + b; 
+    }
+}
