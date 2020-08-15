37d18acf4c6aa945876ef0afc655b1bc4b54961e
salmaeidvelasco <a2019115131@estudiantes.upsa.edu.bo>
2020-08-15 09:37:07 -0400

    Se agregó Multiply, en el suma Controller.

--- Sumar-Restar/Sumar/Controllers/SumaController.cs
+++ Sumar-Restar/Sumar/Controllers/SumaController.cs
@@ -9,19 +9,36 @@ namespace Sumar.Controllers
 {
     [Route("api/[controller]")] // para controlar nuestro servidor
     [ApiController]
-    public class SumaController : ControllerBase
+    public class SumaController : ControllerBase //El nombre con que guardamos antes de Controller ese va después de /api/ sin mayuscula
     {
         [HttpGet]
+        [Route("Add")] //Para saber cual get vamos a correr, le aumentamos una ruta
         public int Add(int a, int b) //Creamos este método público donde también creamos los elementos
         {
             return a + b; //Una vez éstos sumados, enviamos la suma 
+            
         }
 
         [HttpPost]
-        public int Add2([FromHeader] int a, [FromHeader] int b) //Creamos este método público donde también creamos los elementos
+        public int Add2([FromHeader] int a, [FromHeader] int b) 
+            // Fromheader es porque el valor no va a ser como parámetro,el Post no se ve lo que hace en el url osea el a y b
         {
             return a + b;
         }
+        [HttpGet]
+        [Route("Multiply")]
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int Mul2([FromHeader] int a, [FromHeader] int b)
+        {
+            return a * b;
+        }
+
+
     }
 }
 
