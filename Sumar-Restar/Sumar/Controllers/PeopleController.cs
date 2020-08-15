a3e35c89f7237fd73d7602493a0b3a8229ec6009
salmaeidvelasco <a2019115131@estudiantes.upsa.edu.bo>
2020-08-15 10:09:35 -0400

    Mi primer modelo de clase.

---
+++ Sumar-Restar/Sumar/Controllers/PeopleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sumar.Models;
+
+namespace Sumar.Controllers
+{
+    [Route("api/[controller]")]//ctrl r se puede cambiar nombre de la clase o controlador para crear esto pusimos agregagr controlador
+    [ApiController]
+    public class PeopleController : ControllerBase //Mi ruta es People
+    {
+        [HttpGet]
+        public Person Listar() //Creamos el objeto person 
+        {
+            Person person = new Person()// Obejto person de Person
+            {
+                PersonId = 1,
+                Name = "Salma Eid"
+            };
+            return person;
+        }
+    }
+}
---
+++ Sumar-Restar/Sumar/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sumar.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; } //Para crear se hace prop tab tab, luego cambio el tipo de variable y el nombre del método, get y set
+        public string Name { get; set; } 
+
+    }
+}
